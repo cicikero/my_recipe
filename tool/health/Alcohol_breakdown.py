1aeb23fb65a7b4c7ca2edb2925b4494d667443ee
cicikero <cicikero@gmail.com>
2023-05-30 17:06:32 +0900

    古いコードが混ざったので訂正
    古いコードが混ざったので訂正
    仕様もコメントで追記

--- tool/health/Alcohol_breakdown.py
+++ tool/health/Alcohol_breakdown.py
@@ -1,23 +1,14 @@
-def calculate_time_to_breakdown_alcohol():
-    weight = float(input("体重を入力してください(kg): "))
-    alcohol_type = {"ビール": 5, "日本酒": 15, "焼酎": 25, "ワイン": 12} # アルコール度数
-    alcohol_amount = {"ビール": 350, "日本酒": 180, "焼酎": 180, "ワイン": 100} # 単位(ml)
-    alcohol_list = []
-    while True:
-        alcohol = input("飲んだお酒を選択してください(ビール、日本酒、焼酎、ワイン)。終了する場合は「q」を入力してください: ")
-        if alcohol == "q":
-            break
-        amount = float(input(f"{alcohol}の飲んだ量を入力してください(単位: ml): "))
-        alcohol_list.append((alcohol, amount))
-    alcohol_content = sum([alcohol_amount[alcohol] * alcohol_type[alcohol] * amount for alcohol, amount in alcohol_list]) * 0.8 / 100 # 飲んだアルコール量(g)
-    breakdown_alcohol_content = weight * 0.1 # 1時間で分解できるアルコール量(g)
-    breakdown_time = (alcohol_content / (breakdown_alcohol_content * 10)) # アルコールが分解されるまでの時間(時)
-    print(f"飲んだアルコール量は{alcohol_content:.2f}gです。")
-    print(f"アルコールが分解されるまでの時間は{breakdown_time:.2f}時間です。")
+'''
+飲酒後にアルコールが分解されるまでの時間を概算するPythonアプリ
 
-calculate_time_to_breakdown_alcohol()
-
-
+仕様:
+・標準入力で、体重を指定する
+・標準入力で、飲んだお酒と量を指定する
+・アルコール量(g) = 飲んだお酒の量(ml) × アルコール度数(%) × 0.8 ÷ 100
+・1時間で分解できるアルコール量を計算する。計算式は「 体重 (kg) × 0.1 = 1時間で分解できるアルコール量（g） 」
+・選択肢として表示する飲んだ酒の種類、量、含まれるアルコール量は別に定義。定義内容は、酒の種類×単位(本、杯など)
+・最後に摂取したアルコール量と分解にかかる時間(時間単位)で表示
+'''
 
 def calculate_time_to_breakdown_alcohol():
     weight = float(input("体重を入力してください(kg): "))
